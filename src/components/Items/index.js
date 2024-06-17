2d03da7336e1b042f9062ebaf78711e30cf80a16
Henrique William <os.henriquewiliiam@gmail.com>
2024-06-16 21:34:49 -0300

    commit implementando dropdowns

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './items.css';
 
 export const Items = () => {
+  const [filteredItems, setFilteredItems] = useState([]);
+  const allItems = [
+    { name: 'Item 1', category: 'Cozinha' },
+    { name: 'Item 2', category: 'Quarto' },
+    { name: 'Item 3', category: 'Cozinha' },
+    { name: 'Item 4', category: 'Escritório' },
+  ];
+
+  useEffect(() => {
+    // Inicializa com todos os itens
+    setFilteredItems(allItems);
+  }, []);
+
+  const handleFilter = (category) => {
+    if (category === 'Todos') {
+      setFilteredItems(allItems);
+    } else {
+      const filtered = allItems.filter(item => item.category === category);
+      setFilteredItems(filtered);
+    }
+  };
+
   return (
     <div className='items'>
         <div className='items__category'>
 
           {/* Filtro Dropdown */}
           <div className='filter__dropdown'>
-            <div className='dropdown'>
-              <p className='filter__titulo'>CATEGORIAS</p>
-              <button className='filter__button'>Sala de Estar <img src='/images/icon/arrow-down.svg' alt='icone dropdown'/></button>
-            </div>
-
-            <div className='dropdown'>
-              <p className='filter__titulo'>PREÇO</p>
-              <button className='filter__button'>Todos os Preços<img src='/images/icon/arrow-down.svg' alt='icone dropdown'/></button>
-            </div>
-          
+            <Dropdown title='CATEGORIAS' button='Todos' onFilter={handleFilter}/>
+            {/* <Dropdown title='PREÇO' button='Todos os Preços'/>           */}
           </div>
 
           {/* organizar */}
@@ -52,4 +67,47 @@ export const Items = () => {
         </div>
     </div>
   )
-}
+};
+
+export const Dropdown = (props) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(props.button);
+
+  const handleToggle = () => {
+    setIsChecked(prevState => !prevState);
+  };
+
+  const handleItemClick = (item) => {
+    console.log(item);
+    setSelectedItem(item);
+    setIsChecked(false); // Fechar o dropdown após selecionar um item
+    props.onFilter(item); // Chamar a função de filtro passada via props
+  };
+
+  return (
+    <div className='dropdown'>
+      <p className='filter__titulo'>{props.title}</p>
+      <label className='dropdown__label'>
+        <input type='checkbox' checked={isChecked} className='dropdown__input' onChange={handleToggle}/>
+        <p className='dropdown__btn'>
+          {selectedItem}
+          <img 
+              src='/images/icon/arrow-down.svg' 
+              alt='icone dropdown'
+          />
+        </p>
+
+        {isChecked && (
+          <ul className='dropdown__list'>
+            <a onClick={() => handleItemClick('Todos')}><li>Todos</li></a>
+            <a onClick={() => handleItemClick('Cozinha')}><li>Cozinha</li></a>
+            <a onClick={() => handleItemClick('Quarto')}><li>Quarto</li></a>
+            <a onClick={() => handleItemClick('Escritório')}><li>Escritório</li></a>
+            <a onClick={() => handleItemClick('Banheiro')}><li>Banheiro</li></a>
+          </ul>
+        )}
+        
+      </label>
+    </div>
+  );
+};
