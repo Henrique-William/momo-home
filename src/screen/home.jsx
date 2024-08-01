5f4a3073d1ae5757186f4d0f51485419f08d4496
Henrique William <os.henriquewiliiam@gmail.com>
2024-08-01 19:27:44 -0300

    creted enterprises ccomponent

--- src/screen/home.jsx
+++ src/screen/home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import Enterprises from "../components/enterprises"
 
 function Home() {
   return (
     <>
       <Header />
+      <Enterprises/>
     </>
   );
 };
